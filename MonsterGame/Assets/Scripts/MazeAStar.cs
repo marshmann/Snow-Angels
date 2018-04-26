bb77a53c123c61ed3b0ea600a28ae901b2350cbc
marshmann <marshmann@duq.edu>
2018-04-26 12:05:28 -0400

    Split MinPQ and Denumerable into their own scripts
    For my sanity

--- MonsterGame/Assets/Scripts/MazeAStar.cs
+++ MonsterGame/Assets/Scripts/MazeAStar.cs
@@ -252,7 +252,7 @@ public class ANode : IDenumerable, IComparer  {
         if (state.Equals(n.GetState()) && cost == n.GetCost())
             return true;
         return false;
-    }
+        }
 
     int IComparer.Compare(Object o1, Object o2)
     {
@@ -264,108 +264,4 @@ public class ANode : IDenumerable, IComparer  {
             return -1;
         else return 0;
     }
-}
-
-// ***NOTE: The Compare(obj, obj) method is a method of the ANode class right now, so it has to be perfomed on an ANode object... that doesn't seem right.
-
-//Code given by Dr. Simon
-// adapted from Sedgewick.
-
-
-// GENERIC MINPQ: 
-public interface IDenumerable
-{
-    int GetNumber();
-    void SetNumber(int x);
-}
-public class MinPQ<Key> where Key : IComparer, IDenumerable
-{
-    public ArrayList pq;
-    private int N = 0;
-
-    public MinPQ(int cap)
-    {
-        pq = new ArrayList(cap + 1) {
-            null
-        };
-    }
-
-    public MinPQ()
-        : this(1) { }
-
-    public Boolean IsEmpty() { return N == 0; }
-
-    public int Size() { return N; }
-
-    public Key Min()
-    {
-        if (IsEmpty()) throw new Exception("Priority queue underflow");
-        return (Key)pq[1];
-    }
-
-    public void Add(Key x)
-    {
-        N++;
-        pq.Add(x);
-        Swim(N, x);
-    }
-
-    public Key Remove()
-    {
-        if (IsEmpty()) throw new Exception("Priority queue underflow");
-        Key min = (Key)pq[1];
-        Swap(pq, 1, N--);
-        Sink(1);
-        pq.RemoveAt(N + 1);
-        min.SetNumber(0);
-        return min;
-    }
-
-    private void Swim(int k, Key q)
-    {
-        pq[k] = q;
-        Key x = (Key)pq[k]; // assigned outside of loop
-        while (k > 1 && ((Key)pq[k / 2]).Compare((Key)pq[k / 2], x) > 0)
-        {
-            pq[k] = pq[k / 2];
-            ((Key)pq[k / 2]).SetNumber(k);
-            k = k / 2;
-        }
-        pq[k] = x;
-        x.SetNumber(k);
-    }
-
-    private void Sink(int k)
-    {
-        Key x = (Key)pq[k];
-        Key y;
-        while (2 * k <= N)
-        {
-            int j = 2 * k;
-            y = (Key)pq[j];
-            if (j < N && y.Compare(y, (Key)pq[j + 1]) > 0) j++;
-            if (x.Compare(x, (Key)pq[j]) <= 0) break;
-            pq[k] = pq[j];
-            Key key = (Key)pq[j];
-            key.SetNumber(k);
-            k = j;
-        }
-        pq[k] = x;
-        x.SetNumber(k);
-    }
-
-    private static void Swap(ArrayList pq, int i, int j)
-    {
-        Key t = (Key)pq[i];
-        pq[i] = pq[j];
-        pq[j] = t;
-        ((Key)pq[i]).SetNumber(i);
-        ((Key)pq[j]).SetNumber(j);
-    }
-
-    public void Update(Key x)
-    {
-        Swim(x.GetNumber(), x);
-        Sink(x.GetNumber());
-    }      
 }
